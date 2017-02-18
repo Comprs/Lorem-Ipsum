0d6c1c24d087a1c74a3344f451f543f53d3c0d46
InverseSec <Khurramwork@gmail.com>
2017-02-18 05:10:27 +0000

    most of the generation loading from database complete.

--- game/core/src/me/lihq/game/models/Dialogue/Question.java
+++ game/core/src/me/lihq/game/models/Dialogue/Question.java
@@ -11,7 +11,7 @@ public class Question {
     private Style style;
     private String questionText;
 
-    Question(Style style, String text) {
+    public Question(Style style, String text) {
         this.style = style;
         this.questionText = text;
     }
